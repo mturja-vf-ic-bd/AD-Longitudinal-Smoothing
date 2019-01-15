35f0d256fb54c333e828abaf4de2c1ecf0363d1e
mturja-vf-ic-bd <mturja.vf.ic.bd@gmail.com>
2019-01-15 16:53:35 -0500

    RBF fixed

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
 from utils.readFile import *
+from matplotlib import pyplot as plt
 
 def rbf_optimization(S, sigma, lambda_m):
+    S = np.insert(S, len(S), S[len(S) - 1])
+    S = np.insert(S, len(S), S[len(S) - 1])
+    S = np.insert(S, 0, S[0])
+    S = np.insert(S, 0, S[0])
+    print("\nS : ", S)
     n = len(S)
-    W = np.ones(n) * 1/n
-    time_points = [i for i in range(1, len(max_val) + 1)]
+    W = np.ones(n) / n
+    time_points = [i for i in range(1, len(S) + 1)]
     nIter = 1000
     threshold = 10 ** -10
     alpha = 0.2
@@ -11,7 +17,7 @@ def rbf_optimization(S, sigma, lambda_m):
     for it in range(0, nIter):
         print("Iteration : ", it)
         # S_cont = comp_rbf_val(W, time_points, sigma)
-        S_cont = comp_avg3_val(S, W)
+        S_cont = comp_rbf_val(W, time_points, sigma)
         # S_cont = np.exp(S_cont) / sum(S_cont)
         d_z = 2 * (-(S - S_cont) + lambda_m * W)
         if (abs(d_z) > threshold).sum() == 0:
@@ -26,19 +32,15 @@ def comp_rbf_val(W, time_points, sigma):
     W = np.asarray(W)
     time_points = np.asarray(time_points)
     time_points_tiled = np.transpose(np.tile(time_points, (len(W), 1)))
-    T = [i for i in range(0, len(W))]
+
+    T = [i+1 for i in range(0, len(W))]
     T = np.tile(T, (len(time_points), 1))
-    return np.matmul((np.exp(- (time_points_tiled - T) ** 2) / (2 * sigma ** 2)), np.transpose(W))
+    #print('\ntime_points_tiled: ', time_points_tiled,
+    #      '\nT: ', T)
 
+    radial_basis = np.exp(- (time_points_tiled - T) ** 2 / (2 * sigma ** 2))
+    return np.matmul(radial_basis, np.transpose(W))
 
-def comp_avg3_val(S, W):
-    S_cont = [S[0]]
-    for i in range(1, len(S) - 1):
-        S_cont.append(W[i - 1] * S[i - 1] + W[i] * S[i] + W[i + 1] * S[i + 1])
-
-    S_cont.append(S[len(S) - 1])
-
-    return S_cont
 
 
 if __name__ == "__main__":
@@ -49,11 +51,19 @@ if __name__ == "__main__":
         max_val.append(np.amax(mat))
 
     max_val = np.asarray(max_val)
-    sigma = 10
-    lambda_m = 0.4
+    sigma = 2
+    lambda_m = 0.1
+    T = [i+1 for i in range(0, len(max_val))]
+    step=3
+    time_points = [i/step for i in range(step, step*(len(max_val) + 1))]
+
     W = rbf_optimization(max_val, sigma, lambda_m)
-    time_points = [i for i in range(1, len(max_val) + 1)]
-    val = comp_avg3_val(max_val, W)
+
+    val = comp_rbf_val(W, time_points, sigma)
     print("\nactual val: ", max_val,
           "\noutput : ", val,
           "\nW = ", W)
+
+    plt.scatter(T, max_val)
+    plt.plot(time_points, val)
+    plt.show()
